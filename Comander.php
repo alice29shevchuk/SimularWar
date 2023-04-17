755c48f7dce0ea7f4f75e69cc1f16e8788c8ffab
alice29shevchuk <Sevc_sd76@student.itstep.org>
2023-04-17 19:10:10 +0300

    add commander

---
+++ Comander.php
@@ -0,0 +1,9 @@
+<?php
+class Comander extends Warrior{
+    private $achievements;
+    public function __construct()
+    {
+        $this->achievements = array();
+    }
+}
+?>
---
+++ Squad.php
@@ -0,0 +1,36 @@
+<?php
+abstract class Squad{
+    private $minCount;
+    private $maxCount;
+    private $arrWarriors;
+    private $nameSquad;
+    public function __construct($nameSquad)
+    {
+        $this->nameSquad=$nameSquad;
+        $this->minCount=5;
+        $this->maxCount=25;
+        $this->arrWarriors = array();
+
+    }
+    public function addToSquad($warrior)
+    {
+        if(count($this->arrWarriors)<=$this->maxCount)
+        {
+            array_push($this->arrWarriors,$warrior);
+        }
+        else{
+            echo '<p>'.'Отряд переполнен!'.'</p>';
+        }
+    }
+    public function removeFromSquad()
+    {
+        if($this->arrWarriors!=null)
+        {
+            echo "Отряд существует";
+        }
+        else{
+            echo "Отряд не существует";
+        }
+    }
+}
+?>
